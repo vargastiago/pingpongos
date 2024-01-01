566dd6577263a35352d3d98344cac0a765fd1547
Tiago Vargas <tiagovargas@ymail.com>
2024-01-01 13:49:40 -0300

    Adiciona P5 - Preempção por tempo

---
+++ p5-preempcao-por-tempo/ppos_data.h
@@ -0,0 +1,61 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Versão 1.1 -- Julho de 2016
+
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __PPOS_DATA__
+#define __PPOS_DATA__
+
+#include "queue.h"    // biblioteca de filas genéricas
+#include <ucontext.h> // biblioteca POSIX de trocas de contexto
+
+#define STACKSIZE 32768 // tamanho da pilha de threads
+#define AGING_FACTOR 1
+#define MIN_PRIORITY 20
+#define MAX_PRIORITY -20
+#define TICKS 20
+
+typedef enum { NEW,
+               READY,
+               RUNNING,
+               FINISHED } status_t;
+
+// Estrutura que define um Task Control Block (TCB)
+typedef struct task_t {
+    struct task_t *prev, *next; // ponteiros para usar em filas
+    int id;                     // identificador da tarefa
+    ucontext_t context;         // contexto armazenado da tarefa
+    status_t status;
+    int static_prio;
+    int dynamic_prio;
+    int is_sys_task;
+    int quantum;
+    // ... (outros campos serão adicionados mais tarde)
+} task_t;
+
+// estrutura que define um semáforo
+typedef struct
+{
+    // preencher quando necessário
+} semaphore_t;
+
+// estrutura que define um mutex
+typedef struct
+{
+    // preencher quando necessário
+} mutex_t;
+
+// estrutura que define uma barreira
+typedef struct
+{
+    // preencher quando necessário
+} barrier_t;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+    // preencher quando necessário
+} mqueue_t;
+
+#endif
